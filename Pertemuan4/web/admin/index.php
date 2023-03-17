c9385f5de16342911b8484a60f7d135d89fd1a1f
hilmy2583 <muhammadhilmy2583@gmail.com>
2023-03-17 17:35:50 +0700

    Pertemuan Ke-4 menggunakan bootstrap

---
+++ Pertemuan4/web/admin/index.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+
+?>
+
+<div id="layoutSidenav_content">
+  <main>
+    <div class="container-fluid px-4">
+      <h1>Selamat Datang di Halaman Admin</h1>
+      <?php
+      include_once 'dashboard.php';
+      ?>
+    </div>
+  </main>
+</div>
+</div>
+
+<?php
+include_once 'bottom.php';
+
+?>
