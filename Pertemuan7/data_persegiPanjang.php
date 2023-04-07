df1a6262e162f1aed66f9c88c9e633bfd9357f3a
hilmy2583 <muhammadhilmy2583@gmail.com>
2023-04-07 09:35:43 +0700

    update lagi

--- Pertemuan7/data_persegiPanjang.php
+++ Pertemuan7/data_persegiPanjang.php
@@ -4,7 +4,7 @@ require_once 'class_persegiPanjang.php';
 $perPanjang1 = new PersegiPanjang(4, 7);
 $perPanjang2 = new PersegiPanjang(5, 12);
 
-echo '<br>Luas Persegi Panjang I :' . $perPanjang1->getLuas();
+echo 'Luas Persegi Panjang I :' . $perPanjang1->getLuas();
 echo '<br>Luas Persegi Panjang II:' . $perPanjang2->getLuas();
 
 echo '<br>Keliling Persegi Panjang I :' . $perPanjang1->getKeliling();
